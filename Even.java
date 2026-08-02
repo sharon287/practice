9c81ef509b14fe363ed9a57d9e0f87ae158b9be8
sharon287 <sharonabraham2029@cs.sjcetpalai.ac.in>
2026-08-02 12:31:50 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,11 @@
+
+public class Even {
+    public static void main(String[] args) {
+        int i = 2;
+
+        do {
+            System.out.print(i + " ");
+            i += 2;
+        } while (i <= 20);
+    }
+}
